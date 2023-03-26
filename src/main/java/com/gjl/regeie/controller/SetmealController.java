18ad9fae7d500a70be63c725e7de83e3b8a2fc38
gjl <m15100129509@163.com>
2023-03-26 15:17:59 +0800

    使用spring cache 缓存套餐数据

--- src/main/java/com/gjl/regeie/controller/SetmealController.java
+++ src/main/java/com/gjl/regeie/controller/SetmealController.java
@@ -15,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 import sun.print.DialogOnTop;
 
@@ -103,6 +105,7 @@ public class SetmealController {
      */
 
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long>ids){
         log.info("ids:{}",ids);
 
@@ -115,6 +118,7 @@ public class SetmealController {
      */
 
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId + '_' +#setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
 
         LambdaQueryWrapper<Setmeal> queryWrapper =new LambdaQueryWrapper<>();
